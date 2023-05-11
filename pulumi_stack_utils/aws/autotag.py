3cca32b7d0b19700723ad0760731afc8e5cc7a0a
James Arnold <james@rs21.io>
2023-05-11 05:17:43 -0600

    Add type annotations

--- pulumi_stack_utils/aws/autotag.py
+++ pulumi_stack_utils/aws/autotag.py
@@ -1,13 +1,21 @@
 # https://www.pulumi.com/blog/automatically-enforcing-aws-resource-tagging-policies/
 
+from typing import Dict, Union
+
 import pulumi
 
 
-def register_auto_tags(auto_tags):
+def register_auto_tags(auto_tags: Dict[str, str]):
+    """Register a stack transformation that automatically adds tags
+    to resources that support them
+    """
     pulumi.runtime.register_stack_transformation(lambda args: auto_tag(args, auto_tags))
 
 
-def auto_tag(args, auto_tags):
+def auto_tag(
+    args, auto_tags: Dict[str, str]
+) -> Union[pulumi.ResourceTransformationResult, None]:
+    """Automatically add tags to resources that support them"""
     if hasattr(args.resource, "tags"):
         if args.type_ not in ("aws-native:eks:Addon"):
             args.props["tags"] = {**(args.props["tags"] or {}), **auto_tags}
@@ -17,3 +25,4 @@ def auto_tag(args, auto_tags):
                 for k, v in {**(args.props["tags"] or {}), **auto_tags}.items()
             ]
         return pulumi.ResourceTransformationResult(args.props, args.opts)
+    return None
